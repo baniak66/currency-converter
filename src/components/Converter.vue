999ab360fccdb91fda87970a661349fcc5fdc4ce
Pawel Licznar <pawellicznar@gmail.com>
2017-07-13 21:02:27 +0200

    Add multiselect for rates

--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -1,28 +1,47 @@
 <template>
   <div class="converter">
     <h2>Vue.js currency converter</h2>
+    <multiselect v-model="value" :options="rates" :custom-label="codeWithCurr" placeholder="Select one" label="name" ></multiselect>
   </div>
 </template>
 
 <script>
   import axios from 'axios';
+  import Multiselect from 'vue-multiselect';
 
   export default {
+    components: {
+      Multiselect
+    },
     name: 'converter',
     data () {
+      return {
+        rates: [],
+        givenAmount: '',
+        value: { code: '', currency: '', mid: '' },
+      }
     },
     created: function () {
       this.getRates();
     },
     methods: {
       getRates: function () {
+        var vm = this;
         axios.get('http://api.nbp.pl/api/exchangerates/tables/a/')
           .then(function (response) {
-            console.log(response.data[0].no)
+            vm.rates = response.data[0].rates
+            console.log(response.data[0].rates)
           })
           .catch(function (error) {
             console.log(error)
           })
+      },
+      codeWithCurr ({ code, currency }) {
+        if (code == '') {
+          return "Choose currency..."
+        } else {
+          return `${currency} — [${code}]`
+        }
       }
     },
     computed: {
